6c957d9a8628b45f7323ce3eeec5d4acd2358a50
mtt04 <mintin1217@gmail.com>
2023-10-14 17:01:20 -0700

    sentences

---
+++ js/sentences.js
@@ -0,0 +1,5 @@
+const sentences = [
+    "Today is my first ever hackathon.",
+    "I am very nervous as I am not well versed in programming.",
+    "Although I'm so nervous that I couldn't sleep last night, I'm still very excited for what it will entail."
+]
